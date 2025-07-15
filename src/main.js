bf0acfc82c8e3927408a4af27b8ed5b9f0bea9a6
SolDapper <soldapper.com@gmail.com>
2025-07-15 00:55:12 -0400

    modified:   src/main.js

--- src/main.js
+++ src/main.js
@@ -623,6 +623,16 @@ $("body").on('scroll', function() {
 });
 
 
+// get memo for a given escrow
+async function getMemo(escrow){
+
+
+
+
+
+}
+getMemo("78W53qUAAFVkQiscj2HdSECGXpCuZGyBnHHJMUpNGkoD");
+
 // connection events
 async function isConnected(){
     const inWalletApp = await inAppBrowse();
@@ -966,7 +976,6 @@ async function load_public(){
             const asset = splSent.data[i];
             if(asset_mints.includes(asset.token_1_mint) && all_mints.includes(asset.token_3_mint)){
                 if(!$('#market-'+asset.acct).length){
-                    console.log(asset);
                     asset.token_1_details = await asset_map(asset_list,asset.token_1_mint);
                     const merged = token_list.concat(asset_list);
                     asset.token_3_details = await asset_map(merged,asset.token_3_mint);
