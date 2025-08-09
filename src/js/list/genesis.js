70c68d8c0c32236e5e6456b5cc15f52f0c9d67d7
SolDapper <soldapper.com@gmail.com>
2025-08-09 13:02:15 -0400

    modified:   src/js/list/genesis.js

--- src/js/list/genesis.js
+++ src/js/list/genesis.js
@@ -1,12 +1,12 @@
 const genesis_list = [
-    {
-        name: "DefiTuna",
-        symbol: "TUNA",
-        mint: "TUNAfXDZEdQizTMTh3uEvNvYqJmqFHZbEJt8joP4cyx",
-        icon: "https://api.phantom.app/image-proxy/?image=https%3A%2F%2Fmetadata.defituna.com%2Ftuna.png&anim=false&fit=cover&width=128&height=128",
-        pdf: "https://defituna.com/",
-        decimals: 6,
-        issuer: "genesis"
-    }
+    // {
+    //     name: "DefiTuna",
+    //     symbol: "TUNA",
+    //     mint: "TUNAfXDZEdQizTMTh3uEvNvYqJmqFHZbEJt8joP4cyx",
+    //     icon: "",
+    //     pdf: "https://defituna.com/",
+    //     decimals: 6,
+    //     issuer: "genesis"
+    // }
 ];
 export default genesis_list;
